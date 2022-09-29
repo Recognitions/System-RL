8288e649d45b5e94a311426bb6ba8edbf3320a90
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-09-29 13:30:32 -0300

    migrations

--- back/database/migrations/2014_10_12_000000_create_patients_table.php
+++ back/database/migrations/2014_10_12_000000_create_patients_table.php
@@ -13,12 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('patients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('cpf')->unique();
+            $table->string('wpp');
+            $table->string('photo');
+            $table->date('birth');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('patients');
     }
 };
