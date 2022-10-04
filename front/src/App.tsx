8adc1e6a48de543afbc50281cc0800fa7b73c972
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-10-04 13:32:55 -0300

    header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./index.css"
 import {Route,Router,Link,BrowserRouter} from 'react-router-dom'
 import {Header} from './components/Header'
+import {Footer} from './components/Footer'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <main>
                 <b>asds</b>
             </main>
+            <Footer/>
         </BrowserRouter>
     );
 }
